0f92730f7bad53382e8b5f5ddc249fa52391fa39
takaya-47 <local.s.s.47@gmail.com>
2021-02-06 15:04:58 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:create, :index]
+  end
 end
