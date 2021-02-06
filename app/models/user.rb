271bd8ed4a00a7b64bc4dde4b53b5b332c36a2d9
takaya-47 <local.s.s.47@gmail.com>
2021-02-06 10:30:14 +0900

    プルダウンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :rooms, through: :room_users
   has_many :room_users
+  has_many :rooms, through: :room_users
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
