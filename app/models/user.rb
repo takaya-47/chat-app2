85b0351d725728dc4b7c2eaf833cd0b7c74a4525
takaya-47 <local.s.s.47@gmail.com>
2021-02-06 14:24:44 +0900

    messagesテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :room_users
   has_many :rooms, through: :room_users
+  has_many :messages
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
